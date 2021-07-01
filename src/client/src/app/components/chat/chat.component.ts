e55c0cc73aa6f33343b48fec3ed0425d271a7f00
Guillermo Peitzner <guillermopeitzner@gmail.com>
2021-07-01 17:46:14 -0600

    [add] bugs and filter users

--- src/client/src/app/components/chat/chat.component.ts
+++ src/client/src/app/components/chat/chat.component.ts
@@ -12,11 +12,10 @@ import { environment } from 'src/environments/environment';
 })
 export class ChatComponent implements OnInit {
   socket: any = io(environment.socketUrl);
-  messages: Message[] = [
-    { nickname: 'user1', content: 'Hello' },
-    { nickname: 'user1', content: 'World!' },
-  ];
+  messages: Message[] = [];
   message: string = '';
+  friendRoom: string = '';
+  onlineUsers: any[] = [];
 
   constructor(private authService: AuthService, private router: Router) {
     if (!this.authService.user) {
@@ -29,16 +28,30 @@ export class ChatComponent implements OnInit {
       this.socket.on('new-message', (newMessage: Message) => {
         this.messages.push(newMessage);
       });
+      this.socket.on('online-users', (onlineUsers: any[]) => {
+        this.onlineUsers = onlineUsers.filter(
+          (onlineUser) => onlineUser.room !== this.socket.id
+        );
+        console.log('[ONLINE USERS]', this.onlineUsers);
+      });
     }
   }
 
   ngOnInit(): void {}
 
   sendMessage(): void {
-    this.socket.emit('message', {
-      nickname: this.authService.user.nickname,
-      content: this.message,
-    });
-    this.message = '';
+    if (this.friendRoom !== '' && this.message !== '') {
+      const newMessage = {
+        nickname: this.authService.user.nickname,
+        content: this.message,
+      };
+      this.socket.emit('message', newMessage, this.friendRoom);
+      this.messages.push(newMessage);
+      this.message = '';
+    }
+  }
+
+  setFriendRoom(onlineUser: any): void {
+    this.friendRoom = onlineUser.room;
   }
 }
